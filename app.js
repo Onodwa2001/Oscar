be19321698059812146eeb7ee8e0cb173ee36abc
Onodwa2001 <onodwasiyotula7@gmail.com>
2022-07-11 18:00:39 +0200

    Post item and see more details functionality (unstyled)

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth_routes');
 
+// db objects
+const Post = require('./model/Post');
+
 app.set('view engine', 'ejs');
 
 app.use(express.static('static_files'));
@@ -11,12 +14,12 @@ app.use(express.json());
 
 app.listen(8000);
 
-// dbURI = 'mongodb+srv://tom123:Graphic4@mynewcluster.coxgo.mongodb.net/user-cluster?retryWrites=true&w=majority';
-// mongoose.connect(dbURI)
-//     .then((result) => {
+dbURI = 'mongodb+srv://tom123:Graphic4@mynewcluster.coxgo.mongodb.net/oscar_database?retryWrites=true&w=majority';
+mongoose.connect(dbURI)
+    .then((result) => {
 
-//     })
-//     .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
 
 
 // Routes
@@ -30,7 +33,11 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/buy', (req, res) => {
-    res.render('buy.ejs');
+    Post.find()
+        .then((result) => {
+            res.render('buy.ejs', { title: 'Buy', posts: result });
+        })
+        .catch((err) => console.log(err));
 });
 
 app.get('/sell', (req, res) => {
@@ -38,7 +45,25 @@ app.get('/sell', (req, res) => {
 });
 
 // Other handlers
+app.post('/sell', (req, res) => {
+    const post = new Post(req.body);
 
+    post.save()
+        .then((result) => {
+            res.redirect('/buy');
+        })
+        .catch((err) => console.log(err));
+});
+
+app.get('/buy/details/:id', (req, res) => {
+    const prod_id = req.params.id;
+
+    Post.findById(prod_id)
+        .then((result) => {
+            res.render('details.ejs', { result });
+        })
+        .catch((err) => console.log(err));
+});
 
 app.use(authRoutes);
 app.use((req, res) => {
---
+++ model/Post.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    productName: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    cellNumber: {
+        type: Number
+    },
+    otherDetails: {
+        type: String
+    },
+    image: {
+        type: Buffer,
+        required: true
+    }
+}, { timestamps: true });
+
+let Post = mongoose.model('post', postSchema);
+module.exports = Post;
